a4afe19f6064fa498450da7b79142808ae91a818
Gley21 <gley1199@gmail.com>
2021-04-24 18:58:00 -0400

    Script Update
    Added my own code that would fade out the text as you scrolled past it. Also added code for a button that will take you back to the top of the page and refresh it to reload text.

--- docs/mac.js
+++ docs/mac.js
@@ -1,5 +1,5 @@
-$(function(){  // $(document).ready shorthand
-  $('.first-stanza').fadeIn('slow');
+$(function(){  //ensures that the document is loaded before anything runs
+  $('.first-stanza').fadeIn('slow'); //fades first stanza in
 });
 
 $(document).ready(function() {
@@ -13,7 +13,7 @@ $(document).ready(function() {
             var bottom_of_object = $(this).position().top + $(this).outerHeight();
             var bottom_of_window = $(window).scrollTop() + $(window).height();
 
-            /* If the object is completely visible in the window, fade it it */
+            /* If the object is completely visible in the window, fade it in */
             if( bottom_of_window > bottom_of_object ){
 
                 $(this).animate({'opacity':'1'},1500);
@@ -22,6 +22,27 @@ $(document).ready(function() {
 
         });
 
+        /*check location of "p" elements*/
+        $("p").each(function(i){
+
+          var top_of_object = $(this).offset().top; /* finds where top of "p" element is */
+          var top_of_window = $(window).scrollTop(); /* location of scroll from top of page */
+
+          /* If a "p" element is at the top of the window or has been scrolled past, fade it out */
+          if (top_of_window >= top_of_object){
+
+            $(this).animate({'opacity':'0'},1500);
+
+          } 
+
+        });
+
     });
 
 });
+
+function topFunction(){
+  document.body.scrollTop = 0; /* Scrolls to top of doc */
+  document.documentElement.scrollTop = 0; /* For browsers other than Safari */
+  location.reload(); /* Reloads page so text reappears */
+}
